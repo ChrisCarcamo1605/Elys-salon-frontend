16f0c3aed54039867a8bddc175fb0228e9e91c83
ChrisCarcamo7885 <ccarcamo@teamrush.io>
2026-05-18 21:16:42 -0600

    fix: add allowedHosts configuration for preview server

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,8 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: parseInt(process.env.PORT) || 4173,
+    allowedHosts: ['elysalon.shop', 'react-frontend-dev-c187.up.railway.app'],
+
   },
   
 })
